4d69eb747692c31f5a09b81be52ba34e702994af
RobertoCarnauba <betocarnauba@hotmail.com>
2020-09-26 00:42:00 -0300

    Operação de instanciação da base de dados.

---
+++ src/main/java/com/malutech/commets/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.malutech.commets.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.malutech.commets.domain.User;
+import com.malutech.commets.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repo.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		
+		repo.saveAll(Arrays.asList(maria,alex, bob));
+		
+		
+	}
+
+}
