3f6628936d8623d5e9db4a00174ec4f90b4aef68
RobertoCarnauba <betocarnauba@hotmail.com>
2020-09-25 22:16:14 -0300

    Project create

---
+++ src/main/java/com/malutech/commetsservice/ApiCommetsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.malutech.commetsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCommetsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCommetsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malutech/commetsservice/ApiCommetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malutech.commetsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
